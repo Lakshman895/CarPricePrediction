f652c84121c23fb7925293e6c67a8f711f14c81a
Minimaxgithub <lakshman9440823127@gmail.com>
2024-12-22 01:24:49 +0530

    comment

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template, session
+import numpy as np
+import pandas as pd
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+application = Flask(__name__)
+app = application
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html', results=None)
+    else:
+        data = CustomData(
+            Make=request.form.get('Make'),
+            Model=request.form.get('Model'),
+            Year=int(request.form.get('Year')),
+            Mileage=int(request.form.get('Mileage')),
+            Fuel_Type=request.form.get('Fuel_Type'),
+            Engine_Size=float(request.form.get('Engine_Size')),
+            Transmission=request.form.get('Transmission'),
+            Body_Type=request.form.get('Body_Type'),
+            Color=request.form.get('Color'),
+            Owner_History=request.form.get('Owner_History'),
+            Age=int(request.form.get('Age')),
+        )
+        
+        pred_df = data.get_data_as_data_frame()
+        print('The data to be predicted: ', pred_df.T)
+        
+        predict_pipeline = PredictPipeline()
+        print('Got the predict pipeline')
+        log_results = predict_pipeline.predict(pred_df)
+        print('Log result: ', log_results)
+        results = np.expm1(log_results)
+        print('The result is: ', results)
+        return render_template('home.html', results=results[0])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,77 @@
+import sys
+import os
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+    
+    def predict(self, features):
+        model_path = os.path.join('artifacts', 'model.pkl')
+        preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
+        print("Before Loading")
+        
+        # Loading the model and preprocessor
+        model = load_object(file_path=model_path)
+        print("The model is : ", model)
+        
+        preprocessor = load_object(file_path=preprocessor_path)
+        print("After Loading")
+        
+        # Transforming the features
+        data_scaled = preprocessor.transform(features)
+        print("The scaled data is : ", data_scaled.T)
+        
+        # Making predictions
+        preds = model.predict(data_scaled)
+        
+        return preds
+    
+class CustomData:
+    def __init__(self,
+                 Make: str,
+                 Model: str,
+                 Year: int,
+                 Mileage: int,
+                 Fuel_Type: str,
+                 Engine_Size: float,
+                 Transmission: str,
+                 Body_Type: str,
+                 Color: str,
+                 Owner_History: str,
+                 Age: int):
+        
+        self.Make = Make
+        self.Model = Model
+        self.Year = Year
+        self.Mileage = Mileage
+        self.Fuel_Type = Fuel_Type
+        self.Engine_Size = Engine_Size
+        self.Transmission = Transmission
+        self.Body_Type = Body_Type
+        self.Color = Color
+        self.Owner_History = Owner_History
+        self.Age = Age
+        
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                'Make' : [self.Make],
+                'Model' : [self.Model],
+                'Year' : [self.Year],
+                'Mileage' : [self.Mileage],
+                'Fuel_Type' : [self.Fuel_Type],
+                'Engine_Size' : [self.Engine_Size],
+                'Transmission' : [self.Transmission],
+                'Body_Type' : [self.Body_Type],
+                'Color' : [self.Color],
+                'Owner_History' : [self.Owner_History],
+                'Age' : [self.Age]
+            }
+            
+            return pd.DataFrame(custom_data_input_dict)
+        
+        except Exception as e:
+            raise CustomException(e, sys)
