bdd9701c67a5b8f8cfe6e4711d5d582329e58a78
Minimaxgithub <lakshman9440823127@gmail.com>
2024-12-22 14:43:25 +0530

    app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, session
 import numpy as np
 import pandas as pd
+import os
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
@@ -42,4 +43,5 @@ def predict_datapoint():
 
 if __name__ == "__main__":
     #app.run(host="0.0.0.0")
-    app.run(host="0.0.0.0", debug=True)
+    #app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
